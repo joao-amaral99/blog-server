e103529c4d52b8dffcd5108eccc959e0b881c01d
João Amaral <joao.amaral.almeida99@gmail.com>
2022-11-07 21:30:35 -0300

    refactor: change server port

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import { router } from "./routes";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.use(express.urlencoded({ extended: true }));
 app.use(cors);
-app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(3333, () => {
   console.log(`Servidor rodando`);
 });
