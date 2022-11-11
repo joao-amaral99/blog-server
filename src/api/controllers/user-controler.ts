aec8b45c5ccbbea95b92b022cb70a9899e4aa4ff
João Amaral <joao.amaral.almeida99@gmail.com>
2022-11-10 22:57:05 -0300

    feat: add update and delete functions

--- src/api/controllers/user-controler.ts
+++ src/api/controllers/user-controler.ts
@@ -1,12 +1,20 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
+const _select = {
+  id: true,
+  email: true,
+  name: true,
+  password: false,
+};
+
 async function createUser(req: express.Request, res: express.Response) {
   const createUserBody = z.object({
-    email: z.string(),
+    email: z.string().email(),
     name: z.string(),
     password: z.string(),
   });
@@ -16,13 +24,17 @@ async function createUser(req: express.Request, res: express.Response) {
   try {
     const userAlreadyExists = await prisma.user.findFirst({ where: { email } });
 
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(password, saltRounds);
+
     if (!userAlreadyExists) {
       const user = await prisma.user.create({
         data: {
           email,
           name,
-          password,
+          password: hash,
         },
+        select: _select,
       });
 
       return res.status(201).send({ user });
@@ -38,7 +50,9 @@ async function createUser(req: express.Request, res: express.Response) {
 
 async function getAllUsers(req: Request, res: Response) {
   try {
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      select: _select,
+    });
 
     return res.status(200).send(users);
   } catch (error) {
@@ -49,14 +63,17 @@ async function getAllUsers(req: Request, res: Response) {
 }
 
 async function getUserById(req: Request, res: Response) {
-  const createUserBody = z.object({
+  const getIdFromParams = z.object({
     id: z.string(),
   });
 
-  const { id } = createUserBody.parse(req.params);
+  const { id } = getIdFromParams.parse(req.params);
 
   try {
-    const user = await prisma.user.findFirst({ where: { id } });
+    const user = await prisma.user.findFirst({
+      where: { id },
+      select: _select,
+    });
 
     if (!user) {
       return res
@@ -72,4 +89,71 @@ async function getUserById(req: Request, res: Response) {
   }
 }
 
-export { createUser, getAllUsers, getUserById };
+async function updateUser(req: Request, res: Response) {
+  const idUser = z.object({
+    id: z.string(),
+  });
+
+  const createUserBody = z.object({
+    email: z.string().email("O email deve ser válido").optional(),
+    name: z.string().optional(),
+    password: z.string().optional(),
+  });
+
+  const { id } = idUser.parse(req.params);
+
+  const { email, name, password } = createUserBody.parse(req.body);
+
+  try {
+    const user = await prisma.user.findFirst({ where: { id } });
+
+    if (!user) {
+      return res
+        .status(200)
+        .json({ message: "Não existe usuário com esse id" });
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        email,
+        name,
+        password,
+      },
+      select: _select,
+    });
+
+    return res.status(200).send({ updatedUser });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Houve um problema ao atualizar o usuário" });
+  }
+}
+
+async function deleteUser(req: Request, res: Response) {
+  const getIdFromParams = z.object({
+    id: z.string(),
+  });
+
+  const { id } = getIdFromParams.parse(req.params);
+
+  try {
+    const user = await prisma.user.findFirst({ where: { id } });
+
+    if (!user) {
+      return res
+        .status(200)
+        .json({ message: "Não existe usuário com esse id" });
+    }
+
+    await prisma.user.delete({ where: { id } });
+    return res.status(204).send();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Houve um problema para deletar o usuário" });
+  }
+}
+
+export { createUser, getAllUsers, getUserById, deleteUser, updateUser };
