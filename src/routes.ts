4445ab7d58084d62a0bcfe1a71e01a3ac0c6d197
João Amaral <joao.amaral.almeida99@gmail.com>
2022-11-10 22:57:54 -0300

    feat: add route delete and update

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import {
   createUser,
   getAllUsers,
   getUserById,
+  deleteUser,
+  updateUser,
 } from "./api/controllers/user-controler";
 
 const router = express.Router();
@@ -15,5 +17,9 @@ router.get("/users", getAllUsers);
 router.post("/users", createUser);
 
 router.get("/users/:id", getUserById);
+
+router.put("/users/:id", updateUser);
+
+router.delete("/users/:id", deleteUser);
 
 export { router };
